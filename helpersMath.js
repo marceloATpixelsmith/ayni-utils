cf752725963d5298d5bfe1224ac14f38ede65f63
marceloATpixelsmith <144948098+marceloATpixelsmith@users.noreply.github.com>
2025-10-30 13:09:34 -0600

    Update helpersMath.js

--- helpersMath.js
+++ helpersMath.js
@@ -1,16 +1,3 @@
-// js/helpersFormatting.js
-window.AyniUtils = window.AyniUtils || {};
-window.AyniUtils.math = {
-  version: '2025.10.30a',
-  toYmd(d){
-    const x = new Date(d);
-    return `${x.getFullYear()}-${String(x.getMonth()+1).padStart(2,'0')}-${String(x.getDate()).padStart(2,'0')}`;
-  }
-};
-
-
-
-
 const decimalToCents = v => Number(v)*100;        //12.34→1234
 const centsToDecimal = c => Number(((Number(c)||0)/100).toFixed(2)); //1234→12.34
 
